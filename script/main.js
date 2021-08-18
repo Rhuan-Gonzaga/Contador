fcfbc951c29eff63efe59827ef842a78280cb281
Indio santos <rhuangonzaga22@gmail.com>
2021-08-18 13:23:09 -0300

    Projetinho finalizado

--- script/main.js
+++ script/main.js
@@ -1,7 +1,20 @@
 
   $("#getting-started")
-  .countdown("2022/01/01", function(event) {
-    $(this).text(
-      event.strftime('%D days %H:%M:%S')
+  .countdown("2077/01/01", function(event) {
+
+    $("#meses").text(
+      event.strftime('%m')
+    );
+    $("#dias").text(
+      event.strftime('%D')
+    );
+    $("#horas").text(
+      event.strftime('%H')
+    );
+    $("#minutos").text(
+      event.strftime('%M')
+    );
+    $("#segundos").text(
+      event.strftime('%S')
     );
   });
